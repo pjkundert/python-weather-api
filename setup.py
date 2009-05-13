f274295ddf2c12e19ab0b5c32bf8a67260cdcb42
qetzal <qetzal@f8314ed2-3ed7-11de-b232-599c1b3c59e8>
2009-05-13 15:57:02 +0000

    git-svn-id: http://python-weather-api.googlecode.com/svn/trunk@24 f8314ed2-3ed7-11de-b232-599c1b3c59e8

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='yweather',
+		version='0.1',
+		description='',
+		author='Eugene Kaznacheev',
+		author_email='qetzal@gmail.com',
+		url='http://code.google.com/p/python-weather-api/',
+		packages=['yweather'],
+		)
+
