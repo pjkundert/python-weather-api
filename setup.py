0a99b4ef43b224e87b4d0d666e1bb66dfa0a8329
qetzal <qetzal@f8314ed2-3ed7-11de-b232-599c1b3c59e8>
2009-05-28 20:42:34 +0000

    git-svn-id: http://python-weather-api.googlecode.com/svn/trunk@66 f8314ed2-3ed7-11de-b232-599c1b3c59e8

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ __version__ = '0.2'
 
 
 setup(name='pywapi',
-		version=__version__,
-		description='A python wrapper around the Yahoo! Weather, Google Weather and NOAA APIs',
-		author='Eugene Kaznacheev',
-		author_email='qetzal@gmail.com',
-		url='http://code.google.com/p/python-weather-api/',
-		py_modules=['pywapi'],
-		keywords = 'weather api yahoo noaa google',
-		)
+    version=__version__,
+    description='A python wrapper around the Yahoo! Weather, Google Weather, NOAA and GisMeteo APIs',
+    author='Eugene Kaznacheev',
+    author_email='qetzal@gmail.com',
+    url='http://code.google.com/p/python-weather-api/',
+    py_modules=['pywapi'],
+    keywords = 'weather api yahoo noaa google gismeteo',
+)
