bdee201e220418f4adc245b13c20f69b1c3fbd15
qetzal <qetzal@f8314ed2-3ed7-11de-b232-599c1b3c59e8>
2009-05-28 14:04:24 +0000

    git-svn-id: http://python-weather-api.googlecode.com/svn/trunk@62 f8314ed2-3ed7-11de-b232-599c1b3c59e8

--- pywapi.py
+++ pywapi.py
@@ -254,7 +254,7 @@ weather_data: a dictionary of weather data. See http://informer.gismeteo.ru/xml.
 	xml_forecats_node  = dom.getElementsByTagName('FORECAST')
 
         for i  in range(xml_forecats_node.length):
-		forecast = dom.getElementsByTagName('MMWEATHER')[0].getElementsByTagName('REPORT')[0].getElementsByTagName('TOWN')[0].getElementsByTagName('FORECAST').item(i) 
+		forecast = xml_forecats_node.item(i) 
         	_tmp_forecast = {}
 		_tmp_forecast.update(xml_get_attrs(forecast, forecast_tag_attr))			    
 		forecast = xml_forecats_node.item(i)
