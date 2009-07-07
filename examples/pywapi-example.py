15f8596f6b81e3f53b05e890182b97a95e3c7ca2
qetzal <qetzal@f8314ed2-3ed7-11de-b232-599c1b3c59e8>
2009-07-07 14:48:22 +0000

    Gismeteo service has been removed. (continue)
    git-svn-id: http://python-weather-api.googlecode.com/svn/trunk@82 f8314ed2-3ed7-11de-b232-599c1b3c59e8

--- examples/pywapi-example.py
+++ examples/pywapi-example.py
@@ -4,12 +4,9 @@ import string
 google_result = pywapi.get_weather_from_google('10001')
 yahoo_result = pywapi.get_weather_from_yahoo('10001')
 noaa_result = pywapi.get_weather_from_noaa('KJFK')
-gismeteo_result = pywapi.get_weather_from_gismeteo('72503_1')
 
 print "Google says: It is " + string.lower(google_result['current_conditions']['condition']) + " and " + google_result['current_conditions']['temp_c'] + "C now in New York.\n\n"
 
 print "Yahoo says: It is " + string.lower(yahoo_result['condition']['text']) + " and " + yahoo_result['condition']['temp'] + "C now in New York.\n\n"
 
-print "NOAA says: It is " + string.lower(noaa_result['weather']) + " and " + noaa_result['temp_c'] + "C now in New York.\n\n"
-
-print "GisMeteo says: It will be from " + gismeteo_result['forecasts'][0]['temperature']['min'] + "C to " + gismeteo_result['forecasts'][0]['temperature']['max'] + "C  in New York." 
+print "NOAA says: It is " + string.lower(noaa_result['weather']) + " and " + noaa_result['temp_c'] + "C now in New York.\n"
