f33b4c21cbf47fea77c2564e8e44cd0be9c284cf
qetzal <qetzal@f8314ed2-3ed7-11de-b232-599c1b3c59e8>
2009-08-16 12:41:55 +0000

    git-svn-id: http://python-weather-api.googlecode.com/svn/trunk@101 f8314ed2-3ed7-11de-b232-599c1b3c59e8

--- pywapi.py
+++ pywapi.py
@@ -22,7 +22,7 @@
 #OTHER DEALINGS IN THE SOFTWARE.
 
 """
-Fetches weather reports from Google Weather, Yahoo Wheather, NOAA and GisMeteo
+Fetches weather reports from Google Weather, Yahoo Wheather and NOAA
 """
 
 import urllib2, re
